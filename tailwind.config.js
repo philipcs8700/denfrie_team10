f43c85c2e60fc540fb9edc61fafe145b8e608366
anla0009 <anla0009@stud.kea.dk>
2025-05-30 08:47:23 -0400

    font!

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        bigShoulders: ['"Big Shoulders"', 'sans-serif'],
+        poppins: ['"Poppins"', 'sans-serif'],
+        archivo: ['"Archivo"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
